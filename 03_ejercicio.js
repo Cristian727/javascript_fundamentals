1e3441ccf89aec477428411d45025bd63b403129
Cristian <cristian.gonzalez7@hotmail.com>
2023-10-28 21:34:39 +0200

    Ejercicio 3

---
+++ 03_ejercicio.js
@@ -0,0 +1,11 @@
+
+
+const usuario = {
+    nombre: "Pedro",
+    edad: 50,
+    ciudad: "Madrid",
+}
+
+var saludo = "Hola, me llamo " + usuario.nombre + ", tengo " + usuario.edad + " años y soy de " + usuario.ciudad + "."
+
+console.log(saludo)
