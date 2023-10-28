fb07c08445b7ac6f144eb7262469f9c88877e93b
Cristian <cristian.gonzalez7@hotmail.com>
2023-10-28 22:19:24 +0200

    Ejercicio 4

---
+++ 04_ejercicio.js
@@ -0,0 +1,42 @@
+
+
+function sumar(a, b){
+    let resultado;
+    resultado = a + b;
+    return resultado;
+}
+
+console.log(sumar(7, 5))
+
+function restar(a, b){
+    let resultado;
+    resultado = a - b;
+    return resultado;
+}
+
+console.log(restar(10, 3))
+
+function multiplicar(a, b){
+    let resultado;
+    resultado = a * b;
+    return resultado;
+}
+
+console.log(multiplicar(10, 3))
+
+function dividir(a, b){
+    let resultado;
+    resultado = a / b;
+    return resultado;
+}
+
+console.log(dividir(10, 3))
+
+function doble(a){
+    let resultado;
+    resultado = a *2;
+    return resultado;
+}
+
+console.log(doble(10))
+
